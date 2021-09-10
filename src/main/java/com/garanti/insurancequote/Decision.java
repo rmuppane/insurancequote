d130d368e54a4eb980e787fcf22dfd2e5a7df548
rhpamAdmin <>
2021-09-10 10:50:11 +0100

    {/src/main/java/com/garanti/insurancequote/Decision.java}

--- src/main/java/com/garanti/insurancequote/Decision.java
+++ src/main/java/com/garanti/insurancequote/Decision.java
@@ -6,12 +6,24 @@ package com.garanti.insurancequote;
 
 public class Decision implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Decision() {
-    }
+	@org.kie.api.definition.type.Label(value = "uwDecision")
+	private java.lang.String uwDecision;
 
+	public Decision() {
+	}
 
+	public java.lang.String getUwDecision() {
+		return this.uwDecision;
+	}
 
+	public void setUwDecision(java.lang.String uwDecision) {
+		this.uwDecision = uwDecision;
+	}
+
+	public Decision(java.lang.String uwDecision) {
+		this.uwDecision = uwDecision;
+	}
 
 }
